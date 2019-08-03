19810ea3cc730ec06f82f4253b2554fcc6a47dab
Zett-8 <kame0008@gmail.com>
2019-08-03 20:12:02 +0900

    add skills

--- src/components/skills_and_resume.js
+++ src/components/skills_and_resume.js
@@ -2,6 +2,104 @@ import React from 'react'
 
 export default () => {
   return (
-    <div id="skills">skills and resume</div>
+    <div id="SR">
+      <div className="SRWrapper">
+
+        <div className="skills">
+          <p className="skills__title">Basic skills</p>
+          <div className="skills__eachSkills">
+            <div className="sBox">
+              <p className="top">Japanese</p>
+              <p className="under">super fluent</p>
+            </div>
+            <div className="sBox">
+              <p className="top">English</p>
+              <p className="under"> </p>
+            </div>
+            <div className="sBox">
+              <p className="top">German</p>
+              <p className="under">as good as 3 years old</p>
+            </div>
+            <div className="sBox">
+              <p className="top">
+                <span className="arrow">&gt; </span>
+                Translation
+              </p>
+              <p className="under">ENG to JPN</p>
+            </div>
+          </div>
+
+          <div className="skills__desc">
+            sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.
+          </div>
+
+          <p className="skills__title">Creative skills</p>
+          <div className="skills__eachSkills">
+            <div className="sBox">
+              <p className="top">
+                <span className="arrow">&gt; </span>
+                StreetPhoto
+              </p>
+              <p className="under">love of my life</p>
+            </div>
+            <div className="sBox">
+              <p className="top">Document Photo</p>
+            </div>
+            <div className="sBox">
+              <p className="top">
+                <span className="arrow">&gt; </span>
+                Portrait
+              </p>
+              <p className="under">unposed<br />natural</p>
+            </div>
+            <div className="sBox">
+              <p className="top">Music Video</p>
+            </div>
+            <div className="sBox">
+              <p className="top">Short Ad Movie</p>
+            </div>
+          </div>
+
+          <div className="skills__desc">
+            sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.
+          </div>
+
+          <p className="skills__title">Develop skills</p>
+          <div className="skills__eachSkills">
+            <div className="sBox">
+              <p className="top">HTML5</p>
+            </div>
+            <div className="sBox">
+              <p className="top">CSS3</p>
+            </div>
+            <div className="sBox">
+              <p className="top">
+                <span className="arrow">&gt; </span>
+                Javascript
+              </p>
+              <p className="under">React<br />Typescript</p>
+            </div>
+            <div className="sBox">
+              <p className="top">Python</p>
+              <p className="under">Rest Framework</p>
+            </div>
+            <div className="sBox">
+              <p className="top">AWS</p>
+              <p className="under">ECS<br />S3<br />CloudFront<br />Route 53<br />RDS</p>
+            </div>
+          </div>
+
+          <div className="skills__desc">
+            sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.sample.
+          </div>
+        </div>
+
+
+
+        <div className="resume">
+
+        </div>
+      </div>
+    </div>
   )
 }
