8c039af63e90c96c89278fc85b97ce9b1443c61e
Zett-8 <kame0008@gmail.com>
2019-08-09 17:45:36 +0900

    feature pwa

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
 import Router from './router'
 
 import './styles/main.scss'
 
 render(<Router />, document.getElementById('root'))
+
+serviceWorker.register()
